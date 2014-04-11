64c93d30c72862eb6a7417c01771ea87099ff886
Konrad Krenzlin <krenzlin@mail.tu-berlin.de>
2014-04-11 12:27:36 +0200

    added test packages

---
+++ pypackage/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="pypackage",
+    py_modules=['foo'],
+    version='0.1.0',
+    )
---
+++ pypackage2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="pypackage2",
+    packages=['pypackage2'],
+    version='0.1.0',
+    )
